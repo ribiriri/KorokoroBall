f50aac2de3fc94ef63a05555a6b3e6dbd5a23b97
ryotaaa <k019g5002@g.neec.ac.jp>
2022-04-25 13:08:03 +0900

    コロコロボール

---
+++ EN1_2/Assets/hikyou.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hikyou : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("”é‹«‚É‚¢‚é");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
