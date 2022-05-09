edec33363efa3b376d0966e6e0b264c65f2daf68
ryotaaa <k019g5002@g.neec.ac.jp>
2022-05-09 11:54:31 +0900

    ころころ３

---
+++ EN1_3/Assets/CheackHit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheackHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
